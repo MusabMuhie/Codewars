03a424818015855f578543ab3f500547e1450fd9
musabmuhie <musabmuhie@gmail.com>
2022-06-09 14:17:00 -0400

    daily challenges 6/9

---
+++ 8 Kyu/Total-points/main.js
@@ -0,0 +1,39 @@
+/*
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+
+//My Solution
+function points(games) {
+    // your code here
+    let total=0;
+    games.forEach(el=>{
+      let arr=el.split(':').map(Number)
+      if(arr[0]>arr[1]){
+        total+=3
+      }else if(arr[0]==arr[1]){
+        total+=1
+      }else{
+        total+=0
+      }
+    })
+    return total
+  }
+
+//Most Voted
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
