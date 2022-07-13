1759dcd7574f0afa7be2cde00307d12c0faf8530
musabmuhie <musabmuhie@gmail.com>
2022-07-13 14:40:21 -0400

    daily challenges 7/13

---
+++ 7 Kyu/Shortest-word/main.js
@@ -0,0 +1,18 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+ */
+
+
+//My Solution
+function findShort(s){
+    let arr = s.split(' ').sort((a, b) => a.length - b.length);
+    return arr[0].length;
+  }
+
+
+//Most Voted
+function findShort(s){
+    return Math.min.apply(null, s.split(' ').map(w => w.length));
+  }
