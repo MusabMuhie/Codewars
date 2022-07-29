fe7ab0afbe8d21bb99f620d141ae4761f952f094
musabmuhie <musabmuhie@gmail.com>
2022-07-28 20:43:16 -0400

    daily challenges 7/28

---
+++ 8 Kyu/Sum-mixed-array/main.js
@@ -0,0 +1,18 @@
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+
+
+//My Solution
+function sumMix(x){
+    return x.map(Number).reduce((acc,c) => acc + c,0)
+   }
+
+
+//Most Voted
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+  }
