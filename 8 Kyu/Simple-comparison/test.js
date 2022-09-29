65d316e42271c011fc4c74da4ee32f441cdea81d
MusabMuhie <musabmuhie@gmail.com>
2022-09-29 13:20:48 -0400

    tests for solutions

--- 8 Kyu/Simple-comparison/test.js
+++ 8 Kyu/Simple-comparison/test.js
@@ -1,14 +1,21 @@
-/*
-Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
-
-To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
-
-
-*/
-
-
-//My Solution
-function add(a, b){
-	// code here
-  return a==b
-}
+describe("add", () => {
+    it("should work for fixed tests", () => {
+       Test.assertEquals(add('1', 1), true);
+       Test.assertEquals(add(1, '1'), true);
+       Test.assertEquals(add(1, '0'), false);
+       Test.assertEquals(add('11', 11), true);
+       Test.assertEquals(add(12, 12), true);
+       Test.assertEquals(add(120, '021'), false);
+    });
+    it("should work for random tests as well", () => {
+      function add2(a, b){
+    return a == b 
+  }
+      var testOptions = [1,2,3,4,5,'1','2','3','4','5']
+      for (let i = 0; i < 100; i++) {
+        var optionA = testOptions[Math.floor((Math.random() * 10) / 5)];
+        var optionB = testOptions[Math.floor((Math.random() * 10) / 5)];
+        Test.assertEquals(add(optionA, optionB), add2(optionA, optionB));
+      }
+    });
+  });
