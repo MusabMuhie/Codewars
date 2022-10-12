d594490d8aae1045b3ab65b75ec3a3c2fdfd1aab
MusabMuhie <musabmuhie@gmail.com>
2022-10-12 12:06:58 -0400

    daily challenges

---
+++ 8 Kyu/Is-it-palindrome/main.js
@@ -0,0 +1,10 @@
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+*/
+
+
+//My Solution
+function isPalindrome(x) {
+    return x.toLowerCase().split('').reverse().join('') == x.toLowerCase()
+  }
