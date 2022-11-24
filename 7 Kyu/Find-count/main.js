0a8649164721de1a0af6e8878995642004e108f4
MusabMuhie <musabmuhie@gmail.com>
2022-11-23 21:08:59 -0500

    daily challenges

---
+++ 7 Kyu/Find-count/main.js
@@ -0,0 +1,21 @@
+/*
+Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+
+Example
+input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+ouptut: 5 
+The most frequent number in the array is -1 and it occurs 5 times.
+*/
+
+
+
+//My Solution
+function mostFrequentItemCount(collection) {
+    let count=0
+    collection.forEach((el,i,arr)=>{
+      if(arr.filter(e=>e==el).length>count){
+        count=arr.filter(e=>e==el).length
+      }
+    })
+    return count
+  }
