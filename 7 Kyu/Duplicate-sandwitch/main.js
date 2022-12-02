3e97ed7f61e729513a8c28a251a8165de80e9018
MusabMuhie <musabmuhie@gmail.com>
2022-12-01 20:29:37 -0500

    daily challenges

---
+++ 7 Kyu/Duplicate-sandwitch/main.js
@@ -0,0 +1,27 @@
+/*
+In this kata you will be given a list consisting of unique elements except for one thing that appears twice.
+
+Your task is to output a list of everything inbetween both occurrences of this element in the list.
+
+Examples:
+[0, 1, 2, 3, 4, 5, 6, 1, 7, 8] => [2, 3, 4, 5, 6]
+["None", "Hello", "Example", "hello", "None", "Extra"] => ["Hello", "Example", "hello"]
+[0, 0] => []
+[true, false, true] => [false]
+"example" => "xampl"
+Notes
+All elements will be the same datatype.
+All used elements will be primitive.
+*/
+
+
+//My Solution
+function duplicateSandwich(a) {
+    let duplicate;
+    for(i=0;i<a.length;i++){
+      if(a.indexOf(a[i])!=a.lastIndexOf(a[i])){
+        duplicate=a[i]
+      }
+    } 
+   return a.slice((a.indexOf(duplicate)+1), a.lastIndexOf(duplicate))
+}
